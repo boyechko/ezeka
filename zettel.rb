e7e742185cdb3721cff2743749506ac71e1f8962
Richard Boyechko <rb-mercurial@diachronic.net>
2017-12-16 13:19:29 -0800

    (WIP) Renamed Zettelkaesten; Numerus is finished

--- zettel.rb
+++ zettel.rb
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-#      Author: Roderick Hoybach <hoybach-code@diachronic.net>
+#      Author: Richard Boyechko <rb-mercurial@diachronic.net>
 # Description: A collection of methods for working with Zettel
 #     Created: 2016-09-02
 #    Comments:
@@ -8,11 +8,11 @@
 require 'time'
 require 'yaml'
 
-class Zettelkasten
+class Zettelkaesten
   class << self; attr_accessor :root end
   class << self; attr_accessor :ext end
 
-  # Default extension for Zettelkasten files
+  # Default extension for Zettelkaesten files
   @ext = ".txt"
 
   @root = Pathname(ENV['ZETTEL_DIR'] || File.expand_path('~/Dropbox/Zettel'))
@@ -34,15 +34,52 @@ class Zettelkasten
   def self.kasten?(string)
     return true if @kaesten[string]
   end
+
+  # Returns the Kasten that the Zettel path is under
+  def self.kasten_of(path)
+    p = Pathname(path)
+    p = Pathname.pwd() + p unless p.absolute?
+    relv = p.relative_path_from(@root)
+    kasten = relv.each_filename.to_a[0]
+    return kasten if @kaesten[kasten]
+  end
+
+  # Returns the type of the zettel found at the given path
+  def self.zettel_type(path)
+    kasten_of(path) == "main" ? :numerus : :tempus
+  end
+
+  # Returns true if the given path is in the Zettelkaesten
+  def self.includes?(path)
+    return true if kasten_of(path)
+  end
+
+  # Returns the appropriate Zettel subclass (Numerus or Tempus) of the Zettel at
+  # the given path.
+  def self.zettel_at(path)
+    kasten = kasten_of(path)
+    if kasten == "main"
+      return Numerus.new_from_path(path)
+    elsif kasten
+      return Tempus.new_from_path(path)
+    else
+      raise "The file at given path is not a Zettel: #{path}"
+    end
+  end
+
+  # Returns the appropriate Zettel subclass (Numerus or Tempus) of the Zettel
+  # from the given link.
+  def self.zettel_from(link)
+  end
 end
 
 class Zettel
-  attr_reader :type, :name, :kasten, :directory, :path
-  attr_accessor :metadata
-
-  def initialize(name)
-    @name = name
-  end
+  attr_reader :type,            # Zettel type; either :tempus or :numerus
+              :kasten,          # Kasten, as string
+              :slug,            # slug only (i.e. without Kasten)
+              :link,            # full link (i.e. with Kasten, unless main)
+              :path             # full path, as Pathname
+  attr_accessor :metadata       # hash
 
   def initialize_metadata()
     if File.exists?(@path)
@@ -61,7 +98,7 @@ class Zettel
 
   # Generates a YAML block as a string, using inline sequence style. Can't use
   # YAML::to_yaml() because it does not support inline style.
-  def self.to_yaml(hash)
+  def self.to_yaml()
     result = ""
     hash.each do |key, val|
       if val.is_a?(Array)
@@ -76,40 +113,81 @@ end
 
 class Numerus < Zettel
   SLUG_PATTERN = /^([0-9]{3})(-([a-z]+))*$/
+  LINK_PATTERN = SLUG_PATTERN
 
-  @type = :numerus_currens
-  @kasten = :main
-  attr_reader :number, :letters # for numerus currens
+  attr_reader :numerus,         # the number portion of the slug
+              :litterae,        # the letter portion of thes lug
+              :section          # the section of the main Kasten
 
-  def initialize(string)
-    super(name)
-    # Figure out of we're passed a link or a path
-    if File.exists?(string) then
-      path = Pathname(string)
-      @name = path.basename(Zettelkasten.ext).to_s
-    elsif string =~ SLUG_PATTERN
-      @name = string
-      @number = $1.to_i
-      @letters = $3
-    else
-      raise TypeError, "Invalid name for a numerus currens Zettel: '#{name}'"
-    end
-
-    # Set the directory
-    if @number >= 0 and @number <= 99
-      @directory = "#{Zettelkasten.main}/000-099"
-    elsif @number >= 100 and @number <= 999
-      @directory = "#{Zettelkasten.main}/#{@name[0]}00-#{@name[0]}99"
-    else
-      raise "Numerus currens '#{name}' is out of bounds (0-999)"
-    end
-    @path = "#{@directory}/#{@name}#{Zettelkasten.ext}"
-    initialize_metadata
+  #
+  # Custom Constructors
+  #
+  def self.new_from_path(*args)
+    numerus = allocate
+    numerus.init_path(*args)
+    numerus
   end
 
+  def self.new_from_link(*args)
+    numerus = allocate
+    numerus.init_link(*args)
+    numerus
+  end
+
+  #
+  # Custom Initializers
+  #
+  def init_path(path)
+    if Zettelkaesten.includes?(path)
+      init_link(Pathname(path).basename(Zettelkaesten.ext).to_s)
+    else
+      raise "The path is not part of the Zettelkaesten: #{path}"
+    end
+  end
+
+  def init_link(link)
+    if link =~ LINK_PATTERN
+      @slug = link
+      @type = :numerus
+      @kasten = "main"
+      @numerus = $1.to_i
+      @litterae = $3
+      @section = self.class.section_of(@slug)
+      @path = Zettelkaesten.dir(@kasten) + @section + (@slug + Zettelkaesten.ext)
+    else
+      raise "The link does not point to a numerus currens: #{link}"
+    end
+  end
+
+  #
+  # Instance Methods
+  #
+
   # Returns the wiki link target
-  def link_target()
-    return @name
+  def link()
+    return @slug
+  end
+
+  #
+  # Class Methods
+  #
+
+  # Returns the appropriate sub-directory in the main Kasten based on the Zettel
+  # slug.
+  def self.section_of(slug)
+    if slug =~ SLUG_PATTERN
+      num = $1.to_i
+      if num >= 0 and num <= 99
+        return "000-099"
+      elsif num >= 100 and num <= 999
+        return "#{slug[0]}00-#{slug[0]}99"
+      else
+        # Should never get here: SLUG_PATTERN limits the numerus to three digits
+        raise "Numerus currens '#{slug}' is out of bounds (0-999)"
+      end
+    else
+      raise "Slug '#{slug}' is not a numerus currens"
+    end
   end
 
   # Returns true if the link (a string) is a valid link to numerus currens zettel
@@ -117,10 +195,11 @@ class Numerus < Zettel
     return link =~ SLUG_PATTERN ? true : false
   end
 
-  # Returns true if the string is a valid link or path a to numerus currens zettel
-  def self.valid_link_or_path?(string)
-    return string =~ SLUG_PATTERN ||
-           File.basename(string, Zettelkasten.ext) =~ SLUG_PATTERN ? true : false
+  # Returns true if the string is a valid path a to numerus currens zettel
+  def self.valid_path?(string)
+    # FIXME: Hardcoded main kasten path pattern
+    return File.basename(string, Zettelkaesten.ext) =~ SLUG_PATTERN &&
+           File.dirname(string) =~ /main\/\d{3}-\d{3}$/ ? true : false
   end
 end
 
@@ -128,37 +207,37 @@ class Tempus < Zettel
   LINK_PATTERN = /^([a-z]+):(\d{8}T\d{4})$/
   SLUG_PATTERN = /^\d{8}T\d{4}$/
 
-  @type = :iso8601
+  @type = :tempus
   attr_reader :time
 
   def initialize(string)
     # Figure out of we're passed a slug or a path
     if File.exists?(string) && valid_link_or_path?(string) then
-      # FIXME: This would allow paths outside of the Zettelkasten
+      # FIXME: This would allow paths outside of the Zettelkaesten
       path = Pathname(string)
       @kasten = path.dirname.to_s.to_sym
-      @name = path.basename(Zettelkasten.ext).to_s
+      @slug = path.basename(Zettelkaesten.ext).to_s
     elsif string =~ LINK_PATTERN
-      if Zettelkasten.valid_kasten?($1.to_sym)
+      if Zettelkaesten.valid_kasten?($1.to_sym)
         @kasten = $1.to_sym
-        @name = $2
+        @slug = $2
       else
         raise TypeError, "Unknown Kasten: '#{$1.to_sym}'"
       end
     else
-      raise TypeError, "Invalid name for a tempus Zettel: '#{name}'"
+      raise TypeError, "Invalid slug for a tempus Zettel: '#{slug}'"
     end
-    super(name)
+    super(slug)
 
-    @time = Time.parse(name)
-    @directory = "#{Zettelkasten.dir(@kasten)}"
-    @path = "#{@directory}/#{@name}#{Zettelkasten.ext}"
+    @time = Time.parse(slug)
+    @directory = "#{Zettelkaesten.dir(@kasten)}"
+    @path = "#{@directory}/#{@slug}#{Zettelkaesten.ext}"
     initialize_metadata
   end
 
   # Returns the wiki link target
   def link_target()
-    return "#{@kasten.to_s}:#{@name}"
+    return "#{@kasten}:#{@slug}"
   end
 
   # Returns true if the link (a string) is a valid link to tempus zettel
@@ -172,8 +251,8 @@ class Tempus < Zettel
       return true
     else
       path = Pathname(string)
-      if path.basename(Zettelkasten.ext).to_s =~ SLUG_PATTERN &&
-         Zettelkasten.valid_kasten?(path.dirname.split[1].to_s)
+      if path.basename(Zettelkaesten.ext).to_s =~ SLUG_PATTERN &&
+         Zettelkaesten.valid_kasten?(path.dirname.split[1].to_s)
         return true
       else
         return false
