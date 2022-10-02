ef1ce8979e8eb2ed2cad00c86f9fd74e512fc514
Richard Boyechko <code@diachronic.net>
2022-10-02 21:31:01 +0800

    Fix ezeka-normalize-file-name to handle calls from after-save-hook

--- ezeka.el
+++ ezeka.el
@@ -798,50 +798,51 @@ read only."
 (defun ezeka-normalize-file-name (&optional filename metadata)
   "Ensure that FILENAME's captioned name matches the METADATA."
   (interactive (list buffer-file-name))
-  (when (eq :numerus (or (alist-get :type metadata)
-                         (ezeka-kasten-id-type (ezeka-file-kasten filename))))
-    (let* ((base (file-name-base filename))
-           (mdata (or mdata (ezeka-file-metadata filename)))
-           (rubric (alist-get :rubric mdata))
-           (keep-which
-            (unless (string= rubric base)
-              (downcase
-               (read-char-choice
-                (format (concat "Caption in filename and metadata differ:\n"
-                                "[F]ilename: %s\n"
-                                "[M]etadata: %s\n"
-                                "Press [f] to set metadata from filename,\n"
-                                "      [m] to set filename from metadata, or\n"
-                                "      [n] or [q] to do noting: ")
-                        (propertize base 'face 'bold)
-                        (propertize rubric 'face 'bold-italic))
-                '(?f ?F ?m ?M ?n ?N ?q ?Q))))))
-      (cond ((or (string= rubric base)
-                 (member keep-which '(?n ?q)))
-             ;; do nothing
-             )
-            ((= keep-which ?f)
-             (setf (alist-get :label mdata) (ezeka-file-name-label base)
-                   (alist-get :caption mdata) (ezeka-file-name-caption base)
-                   (alist-get :citekey mdata) (ezeka-file-name-citekey base))
-             (ezeka-normalize-header filename mdata))
-            ((= keep-which ?m)
-             (let* ((confirmed (read-string
-                                (format "Current:   %s\nRename to: " base)
-                                rubric nil rubric))
-                    (newname (expand-file-name
-                              (file-name-with-extension
-                               (org-trim confirmed)
-                               ezeka-file-extension)
-                              (file-name-directory filename))))
-               (if (not (and filename (file-exists-p filename)))
-                   (set-visited-file-name newname)
-                 (cond
-                  ((vc-backend filename)
-                   (vc-rename-file filename newname))
-                  (t
-                   (rename-file filename newname t)
-                   (set-visited-file-name newname t t))))))))))
+  (let ((filename (or filename buffer-file-name)))
+    (when (eq :numerus (or (alist-get :type metadata)
+                           (ezeka-kasten-id-type (ezeka-file-kasten filename))))
+      (let* ((base (file-name-base filename))
+             (mdata (or metadata (ezeka-file-metadata filename)))
+             (rubric (alist-get :rubric mdata))
+             (keep-which
+              (unless (string= rubric base)
+                (downcase
+                 (read-char-choice
+                  (format (concat "Caption in filename and metadata differ:\n"
+                                  "[F]ilename: %s\n"
+                                  "[M]etadata: %s\n"
+                                  "Press [f] to set metadata from filename,\n"
+                                  "      [m] to set filename from metadata, or\n"
+                                  "      [n] or [q] to do noting: ")
+                          (propertize base 'face 'bold)
+                          (propertize rubric 'face 'bold-italic))
+                  '(?f ?F ?m ?M ?n ?N ?q ?Q))))))
+        (cond ((or (string= rubric base)
+                   (member keep-which '(?n ?q)))
+               ;; do nothing
+               )
+              ((= keep-which ?f)
+               (setf (alist-get :label mdata) (ezeka-file-name-label base)
+                     (alist-get :caption mdata) (ezeka-file-name-caption base)
+                     (alist-get :citekey mdata) (ezeka-file-name-citekey base))
+               (ezeka-normalize-header filename mdata))
+              ((= keep-which ?m)
+               (let* ((confirmed (read-string
+                                  (format "Current:   %s\nRename to: " base)
+                                  rubric nil rubric))
+                      (newname (expand-file-name
+                                (file-name-with-extension
+                                 (org-trim confirmed)
+                                 ezeka-file-extension)
+                                (file-name-directory filename))))
+                 (if (not (and filename (file-exists-p filename)))
+                     (set-visited-file-name newname)
+                   (cond
+                    ((vc-backend filename)
+                     (vc-rename-file filename newname))
+                    (t
+                     (rename-file filename newname t)
+                     (set-visited-file-name newname t t)))))))))))
 
 ;;;=============================================================================
 ;;; Numerus Currens
