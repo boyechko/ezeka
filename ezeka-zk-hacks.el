fa70b6b34b845f48f2377511f6107df46a6a7b18
Richard Boyechko <code@diachronic.net>
2023-03-23 12:43:22 -0700

    Add ezeka-zk-backlings rather than relying on advising zk-backlinks

--- ezeka-zk-hacks.el
+++ ezeka-zk-hacks.el
@@ -34,13 +34,6 @@
 
 ;;; Code:
 
-(defun adv--backlinks-in-entire-ezeka (func &rest args)
-  "Advice around `zk-backlinks' to look in entire `ezeka-directory'.
-Achieves this by lexically binding binding `zk-directory' and calling
-the original FUNC with ARGS."
-  (let ((zk-directory ezeka-directory))
-    (apply func args)))
-
 (defun adv--zk-group-function (file transform)
   "Replace `zk--group-function' to better TRANSFORM the given FILE.
 See `zk--group-function' for details."
@@ -60,6 +53,22 @@ See `zk--group-function' for details."
   (when (ezeka-note-p file)
     (ezeka-file-name-id file)))
 
+(defun ezeka-zk-backlinks ()
+  "Select from list of all notes that link to the current note.
+Unlike `zk-backlinks', search in the entire `ezeka-directory' as well
+as list the note's children."
+  (interactive)
+  (let* ((id (zk--file-id buffer-file-name))
+         (zk-directory ezeka-directory)
+         (files (zk--grep-file-list
+                 (concat "(parent: " id
+                         "|" (regexp-quote (format zk-link-format id))
+                         ")")
+                 t)))
+    (if files
+        (find-file (funcall zk-select-file-function "Backlinks: " files))
+      (user-error "No backlinks found"))))
+
 (defun ezeka-zk-file-name-regexp ()
   "Return the correct regexp matching Ezeka file names.
 The regexp captures these groups:
@@ -73,6 +82,7 @@ Group 2 is the title."
           "$"))
 
 (defvar ezeka-zk-hacks--zfnr-func nil)
+(defvar ezeka-zk-hacks--zb-func nil)
 (defvar ezeka-zk-hacks-mode nil)
 
 (defun ezeka-zk-hacks-mode (&optional arg)
@@ -83,19 +93,21 @@ This is just a pseudo mode."
   (cond ((or (eq arg t)                 ; enable the mode
              (and (numberp arg) (> arg 0))
              (not ezeka-zk-hacks-mode))
-         (advice-add 'zk-backlinks :around 'adv--backlinks-in-entire-ezeka)
          (advice-add 'zk--group-function :override 'adv--zk-group-function)
          (advice-add 'zk--file-id :override 'ezeka-zk--file-id)
          (setq ezeka-zk-hacks--zfnr-func (symbol-function 'zk-file-name-regexp)
+               ezeka-zk-hacks--zb-func (symbol-function 'zk-backlinks)
                ezeka-zk-hacks-mode t)
          (defalias 'zk-file-name-regexp 'ezeka-zk-file-name-regexp)
+         (defalias 'zk-backlinks 'ezeka-zk-backlinks)
          (message "Ezeka-zk-hacks-mode enabled"))
         (t                              ; disable the mode
-         (advice-remove 'zk-backlinks 'adv--backlinks-in-entire-ezeka)
          (advice-remove 'zk--group-function 'adv--zk-group-function)
          (advice-remove 'zk--file-id 'ezeka-zk--file-id)
          (fset 'zk-file-name-regexp ezeka-zk-hacks--zfnr-func)
+         (fset 'zk-backlinks ezeka-zk-hacks--zb-func)
          (setq ezeka-zk-hacks--zfnr-func nil
+               ezeka-zk-hacks--zb-func nil
                ezeka-zk-hacks-mode nil)
          (message "Ezeka-zk-hacks-mode disabled"))))
 
